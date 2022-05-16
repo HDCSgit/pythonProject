facc019ef287d835d3e7265f49f5dff3932b5e15
逍遥游 <1244716205@qq.com>
2022-05-16 17:15:33 +0800

    1

--- 进阶/01.py
+++ 进阶/01.py
@@ -2,3 +2,10 @@ from enum import IntEnum
 
 class StudentEnum(IntEnum):
 	NAME, AGE, SEX, EMAIL = range(4)
+s = ('jim', 16, 'male', 'jim8721@gmail.com')
+t = (34, 'liushuo')
+StudentEnum.NAME
+
+s[StudentEnum.NAME]
+
+from random import randint
